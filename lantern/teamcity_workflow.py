1e9f1764c60bc0695c0976b4fef86827f6186f58
David Echols <dhechols@gmail.com>
2015-07-27 10:46:43 -0700

    Improving teamcity_workflow

--- lantern/teamcity_workflow.py
+++ lantern/teamcity_workflow.py
@@ -8,24 +8,33 @@ def write_text_results(data, outfilename, encoding="utf-8"):
         print("Data written to {0}.".format(outfilename))
 
 
-def write_binary_results(data, outfilename):
-    with open(outfilename, "wb") as f:
-        f.write(data)
-        print("Data written to {0}.".format(outfilename))
-
-
 username = os.environ['veracode_user']
 password = os.environ['veracode_password']
 app_name =  os.environ['veracode_app_name']
-binaries_dir = os.environ['workspace']+ '/output'
+binaries_dir = os.environ['workspace']+ '/output/static_security_build'
 build_name = os.environ['BUILD_NUMBER']
-local_scan_delay = int(60)
+local_scan_delay = 60
 
-global_black_list = ['*.DS_Store']
+global_black_list = [
+    '*.[Ii]nstall*', '*.*manifest*', '*.sql*', '*Test*.*', '*.chm', '*.config*', '*.cs', '*.dacpac', '*.dbschema',
+    '*.deployment', '*.dgml', '*.DS_Store', '*.fakesconfig', '*.fs', '*.ini', '*.lastcodeanalysissucceeded', '*.mht',
+    '*.ndoc', '*.resx', '*.sh', '*.sqp', '*.targets', '*.txt', '*.vsto', '*.xml', '*.xlsx', '*.xsd', 'Antlr*',
+    'AxInterop.*', 'C1.*', 'Castle.Core.*', 'com.contoso.*', 'com.google.*', 'com.sshtool.*', 'CommonBE.*',
+    'CrystalDecisions.*', 'CuteEditor.*', 'DevExpress.*', 'DocumentFormat.*', 'FluentNHibernate.*', 'kodo.*',
+    'Infragistics2.*', 'Interop.*', 'Ionic.Zip.*', 'log4net.*', 'Mabry.*', '[Mm]icrosoft.*', '*.Moles.dll',
+    'net.sf.jmimemagic.*', 'org.apache.*', 'org.drools.*', 'org.jboss.*', 'org.springframework.*', 'PolicyBE.*',
+    'StructureMap.*', '[Ss]ystem.*', 'Telerik*', 'WSAPI*.dll', 'Abyss.dll', 'AjaxControlToolkit.dll',
+    'AuthenticationInterfaces.dll', 'BackgroundCopyManager.dll', 'bii.exe', 'Coalesys.WebMenu.dll',
+    'EntityFramework.dll', 'ICSharpCode.SharpZipLib.dll', 'IRQuery.dll', 'jscoverage.exe', 'LightLogger.dll',
+    'Moq.dll', 'NetSpell.SpellChecker.dll', 'nunit.*', 'Office.dll', 'ojdbc6-*.jar', 'ONSMessages.dll',
+    'PublicDomain.dll', 'QASupport.dll', 'Rhino.Mocks.dll', 'RouteDebug.dll', 'SEAREACH4.DLL', 'ServiceStack.Text.dll',
+    'SourceSafeTypeLib.dll', 'SqlPackage.exe', 'stdole.dll', 'TallComponents.PDF.Layout.dll',
+    'WebDriver.dll', 'weblogic.*', 'Xceed*', 'XPCommonControls.dll']
+
 v = lantern.API(username, password, app_name, build_name)
 v.upload_file_retry(binaries_dir, global_black_list)
 v.begin_prescan(v.app_id, v.build_id, auto_scan=True)
 prescan_results_filename = "{0}-PrescanResults-{1}.xml".format(app_name, build_name)
-# try getting the results. Poll the Veracode service with an exponential backoff until we get results or until it fails.
+# try getting the prescan results. Poll the Veracode service with an exponential backoff until we get results or until it fails.
 prescan_results_xml = v.get_prescan_results(10, 0, 60, 2)  # max wait time = 34.1 hours + local_scan_delay
 write_text_results(prescan_results_xml, prescan_results_filename)
